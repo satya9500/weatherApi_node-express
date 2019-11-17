0c011e7dd5ed8270d7a56c721f6c18d66f69b2d5
satya9500 <satya.prakash9500@gmail.com>
2019-11-18 00:05:38 +0530

    first phase complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var request = require("request");
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +12,25 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  res.render("index");
-  console.log(req.body.city);
+  let city = req.body.city;
+  let apiKey = "d7b76f7926da88ebcd84870ab034ff23";
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  request(url, function(err, response, body) {
+    if (err) {
+      res.render("index", { weather: null, error: "Error, Please Try again" });
+    } else {
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render("index", {
+          weather: null,
+          error: "Error, Please Try again"
+        });
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render("index", { weather: weatherText, error: null });
+      }
+    }
+  });
 });
 
 app.listen(3000, function() {
