972a2109f1e08ccf2a068b24438d91b4272dabdc
satya9500 <satya.prakash9500@gmail.com>
2019-11-17 23:14:50 +0530

    post route for form added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
   res.render("index");
+});
+
+app.post("/", function(req, res) {
+  res.render("index");
+  console.log(req.body.city);
 });
 
 app.listen(3000, function() {
